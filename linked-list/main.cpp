b6598c7ebfd405b9329fa4c6e934054b9b7dc927
Vitor <vitormormul@gmail.com>
2021-02-23 18:42:23 -0300

    Adding test.

--- linked-list/main.cpp
+++ linked-list/main.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+bool test_node()
+{
+    int expected = 3;
+    Node node = Node(3);
+    return node.getData() == expected;
+}
+
 int main()
 {
+    cout << test_node();
 };
